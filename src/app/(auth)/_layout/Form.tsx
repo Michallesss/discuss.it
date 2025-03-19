fcc3b2153ac352166119118f22d4203b4510b47b
Michallesss <mwiemszra@gmail.com>
2025-03-19 14:01:27 +0100

    🏗️ sign in/up layout

--- src/app/(auth)/_layout/Form.tsx
+++ src/app/(auth)/_layout/Form.tsx
@@ -1,1 +1,26 @@
-export default function Form({ children }) {}
+type Props = {
+  children: React.ReactNode;
+  label: string;
+}
+
+export default function Form({ children, label }: Props) {
+  return (
+    <section className="bg-gray-50 dark:bg-gray-950">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+          Discuss.it
+        </a> */}
+        <div
+          className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              {label}
+            </h1>
+            {children}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
