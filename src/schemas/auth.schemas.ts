cb0444cda320e7704140ec150b578fcb87562c30
Michallesss <mwiemszra@gmail.com>
2025-04-03 20:38:38 +0200

    🚚 Rename identifier to email (for normalization)

--- src/schemas/auth.schemas.ts
+++ src/schemas/auth.schemas.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const loginSchema = z.object({
-  identifier: z
+  email: z
     .string({ message: "Passed identifier not seems to be string." }) // ? Is string checked for alphanumeric characters
     .trim()
     .min(3, { message: "Identifier needs to be at least 3 characters long." })
@@ -9,7 +9,6 @@ export const loginSchema = z.object({
   password: z
     .string({ message: "Passed password not seems to be string." }) // ? Is string checked for alphanumeric characters
     .trim()
-    .regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{,}$/, { message: "Password must contain at least one letter, one number and one special character." })
     .min(8, { message: "Password needs to be at least 8 characters long." })
     .max(128, { message: "Password needs to be at most 128 characters long." }),
 });
